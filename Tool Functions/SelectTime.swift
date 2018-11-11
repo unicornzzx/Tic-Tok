506dc5e49ab7fc68b1f2b038fbf771b6b7b5837e
Zhixiang Zhang <zhixiang.zhang0217@hotmail.com>
2018-11-12 00:41:00 +0800

    a useful tool method

---
+++ Tool Functions/SelectTime.swift
@@ -0,0 +1,25 @@
+//
+//  SelectTime.swift
+//  Tic-Tac
+//
+//  Created by unicorn on 2018/5/14.
+//  Copyright © 2018 Echo. All rights reserved.
+//
+
+import UIKit
+
+class SelectTime {
+    static func dateAndTimePicker() -> UIDatePicker{
+        let datePicker = UIDatePicker( )
+        datePicker.datePickerMode = UIDatePickerMode.dateAndTime
+        datePicker.date = NSDate() as Date
+        return datePicker
+    }
+    
+    static func timerPicker() -> UIDatePicker{
+        let datePicker = UIDatePicker( )
+        datePicker.datePickerMode = UIDatePickerMode.countDownTimer
+        datePicker.date = NSDate() as Date
+        return datePicker
+    }
+}
